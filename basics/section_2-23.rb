33040b36f2356b026a68685fd4988636896a0cd4
Sean Ho <seanho96@yahoo.com>
2021-10-28 21:04:06 +0800

    feat: add section 2 video 23 notes

---
+++ basics/section_2-23.rb
@@ -0,0 +1,42 @@
+def multiply(first_num, second_num)
+  # last evaluated expression of the method is implicitly returned
+  # we can also explicitly use the `return` keyword
+  first_num * second_num
+end
+
+puts multiply(2, 5)
+
+############
+
+def operator(first_num, second_num, operation)
+  if operation == "multiply"
+    first_num * second_num
+  elsif operation == "divide"
+    first_num / second_num
+  elsif operation == "add"
+    first_num + second_num
+  elsif operation == "substract"
+    first_num - second_num
+  elsif operation == "modulus"
+    first_num % second_num
+  end
+end
+
+puts "Simple Calculator"
+20.times { print "-" }
+puts
+puts "Please enter the first number"
+num_1 = gets.chomp.to_f
+puts "Please enter the second number"
+num_2 = gets.chomp.to_f
+
+puts "The first number multiplied by the second number is #{operator(num_1, num_2, "multiply")}"
+puts "The first number added by the second number is #{operator(num_1, num_2, "add")}"
+puts "The first number substracted by the second number is #{operator(num_1, num_2, "substract")}"
+if num_2 == 0
+  puts "#{num_1} cannot be divided by #{num_2}"
+  puts "#{num_1} cannot be modulus by #{num_2}"
+else
+  puts "The first number divided by the second number is #{operator(num_1, num_2, "divide")}"
+  puts "The first number modulus by the second number is #{operator(num_1, num_2, "modulus")}"
+end
