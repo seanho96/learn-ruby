12d69343dc9cf61b5d98af410c4be07c85bd0e13
Sean Ho <seanho96@yahoo.com>
2021-11-06 14:55:17 +0800

    feat: add section 2 video 30-31 task

---
+++ basics/section_2-30-31.rb
@@ -0,0 +1,40 @@
+users = [
+  { username: "sean", password: "password1" },
+  { username: "test", password: "password2" },
+  { username: "demo", password: "password3" },
+  { username: "account", password: "password4" },
+  { username: "ho", password: "password5" }
+]
+
+def auth_user(username, password, list_of_users)
+  list_of_users.each do |user|
+    return user if user[:username] == username && user[:password] == password
+  end
+  return "Credentials were not correct"
+end
+
+puts "Welcome to the authenticator"
+40.times { print "-" }
+puts
+puts "This program will take input from the user and compare password"
+puts "If password is correct, you will get back the user object"
+40.times { print "-" }
+puts
+
+attempts = 1
+max_number_of_attempts = 4
+while attempts < max_number_of_attempts
+  print "Username: "
+  username = gets.chomp
+  print "Password: "
+  password = gets.chomp
+  auth_message = auth_user(username, password, users)
+  puts auth_message
+  puts "Press n to quit or any other key to continue: "
+  input = gets.chomp.downcase
+  break if input == "n"
+  attempts += 1
+end
+if attempts == max_number_of_attempts
+  puts "You have exceeded the number of attempts"
+end
