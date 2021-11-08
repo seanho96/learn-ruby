dbfc8e9aa9827beea90e5cdb1cd63fc71ce5c044
Sean Ho <seanho96@yahoo.com>
2021-11-08 16:43:09 +0800

    refactor: each to map and replace Crud with self

--- basics/crud.rb
+++ basics/crud.rb
@@ -1,22 +1,22 @@
 module Crud
   require "bcrypt"
 
-  def Crud.create_hash_digest(password)
+  def self.create_hash_digest(password)
     BCrypt::Password.create(password)
   end
 
-  def Crud.verify_hash_digest(password)
+  def self.verify_hash_digest(password)
     BCrypt::Password.new(password)
   end
 
-  def Crud.create_secure_users(list_of_users)
-    list_of_users.each do |user_record|
-      user_record[:password] = create_hash_digest(user_record[:password])
+  def self.create_secure_users(users)
+    users.map do |user|
+      user[:password] = create_hash_digest(user[:password])
+      user
     end
-    return list_of_users
   end
 
-  def Crud.authenticate_user(username, password, list_of_users)
+  def self.authenticate_user(username, password, list_of_users)
     list_of_users.find do |user_record|
       user_record[:username] == username && user_record[:password] == password
     end || "Credentials were not correct"
